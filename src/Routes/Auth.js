f172095381984538cafd95fe4404789ec8a2417f
ssukim <tntntn0930@gmail.com>
2021-01-04 16:42:27 +0900

    Auth Form with Hooks

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,22 +1,45 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Input from "../Components/Input";
+import Button from "../Components/Button";
+import useInput from "../Hooks/useInput";
 
 const Wrapper = styled.div`
   min-height: 80vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const Box = styled.div`
   ${(props) => props.theme.whiteBox}
   border-radius:0px;
-  width: 350px;
+  width: 100%;
+  max-width: 350px;
 `;
 
 const StateChanger = styled(Box)`
   text-align: center;
   padding: 20px 0px;
+`;
+
+const Form = styled(Box)`
+  padding: 40px;
+  padding-bottom: 30px;
+  margin-bottom: 15px;
+  form {
+    width: 100%;
+    input {
+      width: 100%;
+      &:not(:last-child) {
+        margin-bottom: 7px;
+      }
+    }
+    button {
+      margin-top: 10px;
+    }
+  }
 `;
 
 const Link = styled.span`
@@ -26,9 +49,32 @@ const Link = styled.span`
 
 export default () => {
   const [action, setAction] = useState("logIn");
+  const userName = useInput("");
+  const password = useInput("");
+  const firstName = useInput("");
+  const lastName = useInput("");
+  const email = useInput("");
 
   return (
     <Wrapper>
+       <Form>
+        {action === "logIn" ? (
+          <form>
+            <Input placeholder={"Username"} {...userName} />
+            <Input placeholder={"Password"} {...password} type="password" />
+            <Button text={"Log in"} />
+          </form>
+        ) : (
+          <form>
+            <Input placeholder={"First name"} {...firstName} />
+            <Input placeholder={"Last name"}  {...lastName} />
+            <Input placeholder={"Email"} {...email} type="email"/>
+            <Input placeholder={"Username"} {...userName} />
+            <Input placeholder={"Password"} {...password} type="password" />
+            <Button text={"Sign up"} />
+          </form>
+        )}
+      </Form>
       <StateChanger>
         {action === "logIn" ? (
           <>
